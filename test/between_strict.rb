0a1eb06e3872cc080477519c395ddb802766a6db
itggot-mans-jakobi <38528626+itggot-mans-jakobi@users.noreply.github.com>
2018-04-26 14:23:59 +0200

    Added function between_strict
    Added, tested and documented function between_strict

--- test/between_strict.rb
+++ test/between_strict.rb
@@ -1,4 +1,4 @@
-# Public: Cheks if the first number occurs inbetween and equal to the later two numbers.
+# Public: Cheks if the first number occurs inbetween and not equal to the later two numbers.
 #
 # number1  - The integer that could be between number2 and number3.
 # number2  - The integer that marks the first boundry marker.
@@ -9,10 +9,10 @@
 #   between(1, 3, 5)
 #   # => true
 #
-# Returns true or false depending on if the number is within or equal the two boundries.
-def between(number1, number2, number3)
+# Returns true or false depending on if the number is within the two boundries.
+def between_strict(number1, number2, number3)
     output = false
-    if number1 > (number2 - 1) && number1 < (number3 + 1)
+    if number1 > (number2) && number1 < (number3)
         output = true
     end
     return output 
