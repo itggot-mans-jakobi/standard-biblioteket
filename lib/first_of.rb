1602f61f4b150012cbad957a8dc6f6b28b2ef902
itggot-mans-jakobi <38528626+itggot-mans-jakobi@users.noreply.github.com>
2018-04-26 17:11:57 +0200

    Added function first_of
    Added, tested and documented function first_of

---
+++ lib/first_of.rb
@@ -0,0 +1,13 @@
+# Public: Returns the first element in an array.
+#
+# array - a array.
+#
+# Examples
+#
+#   first_of([1, 2, 3])
+#   # => 1
+#
+# Returns the first item in an array.
+def first_of(array)
+    return array[0] 
+end
