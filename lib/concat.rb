7328800d4b3431a9e0109212bd97ae6a192f16c7
itggot-mans-jakobi <38528626+itggot-mans-jakobi@users.noreply.github.com>
2018-04-26 17:43:15 +0200

    Added function concat
    Added, tested and deocumented function concat

---
+++ lib/concat.rb
@@ -0,0 +1,21 @@
+# Public: Adds together two arrays.
+#
+# array1 - first array.
+# array2 - second array
+#
+# Examples
+#
+#   concat([1, 2, 3], [4, 5, 6])
+#   # => [1, 2, 3, 4, 5, 6]
+#
+# Returns a new array where both arrays are put together.
+def concat(array1, array2)
+    i = 0
+    output = array1
+    while i < array2.length
+        output << array2[i]
+        i += 1
+    end
+    p output
+    return output
+end
