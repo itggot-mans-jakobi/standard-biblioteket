a976499417cc040298d56a2985b5d17522f3057c
itggot-mans-jakobi <38528626+itggot-mans-jakobi@users.noreply.github.com>
2018-04-26 17:19:22 +0200

    Added function last_of
    Added, tested, documented function last_of

---
+++ lib/last_of.rb
@@ -0,0 +1,13 @@
+# Public: Returns the last element in an array.
+#
+# array - a array.
+#
+# Examples
+#
+#   last_of([1, 2, 3])
+#   # => 3
+#
+# Returns the last item in an array.
+def last_of(array)
+    return array[-1]
+end
