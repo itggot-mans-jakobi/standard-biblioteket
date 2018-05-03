0ea9cf5f9ad0608c03737b74cd0525d25f375349
itggot-mans-jakobi <38528626+itggot-mans-jakobi@users.noreply.github.com>
2018-05-03 13:24:18 +0200

    Added function sum
    Added, tested and documented function sum

---
+++ lib/sum.rb
@@ -0,0 +1,21 @@
+# Public: Adds toggeter all the integr:s in an array.
+#
+# array - a array with integr:s in it.
+#
+# Examples
+#
+#   prepend([1, 2, 3, 4])
+#   # => 15
+#
+# Returns the sum of all the integr:s in the array.
+def sum(array)
+    i = 0
+    output = 0
+    while i < array.length
+        output += array[i].to_int
+        i += 1
+    end
+    return output
+end
+
+puts sum([1, 2, 3, 4, 5])
